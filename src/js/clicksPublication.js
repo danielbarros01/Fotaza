168fa45ee331de4081fce667d2e9e3a3d28d03af
danielbarros01 <danibelgranocab@hotmail.com>
2023-07-13 22:19:15 -0300

    Visual options y derecho de uso info

---
+++ src/js/clicksPublication.js
@@ -0,0 +1,15 @@
+const $btnInfo = document.getElementById('btnInfoRightOfUse')
+const $divInfoRightOfUse = document.getElementById('infoRightOfUse')
+
+const $btnOptions = document.getElementById('btnOptions')
+const $optionsPublication = document.getElementById('optionsPublication')
+
+
+$btnInfo.addEventListener('click', () => {
+    $divInfoRightOfUse.classList.toggle('hidden')
+})
+
+$btnOptions.addEventListener('click', () => {
+    $optionsPublication.classList.toggle('hidden')
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ export default {
         sendCategories: './src/js/sendCategories.js',
         addPublication: './src/js/addPublication.js',
         rating: './src/js/rating.js',
+        clicksPublication: './src/js/clicksPublication.js',
     },
     output:{
         filename: '[name].js',
