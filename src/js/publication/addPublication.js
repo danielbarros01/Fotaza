15cc6ac43d9e455d97404315119609e13400f983
danielbarros01 <danibelgranocab@hotmail.com>
2023-08-01 18:49:21 -0300

    Mejora estetica de preview de imagen

--- src/js/publication/addPublication.js
+++ src/js/publication/addPublication.js
@@ -3,6 +3,10 @@ import { maxTags, deleteTag, createTagElement, addTags } from './tags.js'
 
 const $form = document.querySelector("#form")
 const $image = document.querySelector("#image")
+const $infoImg = document.querySelector(".infoImg")
+const $resolutionImg = $infoImg.querySelector("#resolutionImg")
+const $sizeImg = $infoImg.querySelector("#sizeImg")
+const $btnCancelImg = document.querySelector("#deleteImg")
 const $file = document.querySelector("#file")
 const $title = document.querySelector("#title")
 const $categories = document.querySelector("#categories")
@@ -20,7 +24,7 @@ const tags = []
 
 $file.addEventListener('change', () => {
     const formData = new FormData($form)
-    renderImage(formData)
+    renderImage(formData, handleImageUpload)
 })
 
 /* ENVIO */
@@ -80,24 +84,49 @@ $categories.addEventListener('change', function (e) {
     }
 });
 
+$btnCancelImg.addEventListener('click', function(e){
+    e.preventDefault()
+    $file.value = ''
+    $image.setAttribute('src', null)
+    $image.classList.add('hidden')
+    $infoImg.classList.add('hidden')
+    $btnCancelImg.classList.add('hidden')
+
+    $sizeImg.textContent = null
+    $resolutionImg.textContent = null
+})
+
 addTags(tags, $tag, $tags, $spanErrTag);
 
-function renderImage(formData) {
+async function renderImage(formData, cb) {
     const file = formData.get('image')
 
     // Validar el tipo de archivo
     const fileType = file.type;
-
+    console.log(file)
     if (!fileType.startsWith('image/')) {
         $spanErrImg.textContent = 'El archivo seleccionado no es una imagen válida'
         $spanErrImg.classList.remove('hidden')
         $file.value = ''; // Limpiar el input de archivo
         $image.setAttribute('src', ''); // Limpiar la imagen previa
+
+        $infoImg.classList.add('hidden')
+        $btnCancelImg.classList.add('hidden')
+
+        $sizeImg.textContent = null
+        $resolutionImg.textContent = null
     } else {
         const image = URL.createObjectURL(file)
         $image.setAttribute('src', image)
+        $image.classList.remove('hidden')
         $spanErrImg.textContent = ''
         $spanErrImg.classList.add('hidden')
+
+        $infoImg.classList.remove('hidden')
+        $btnCancelImg.classList.remove('hidden')
+
+        $sizeImg.textContent = sizeImg(file.size)
+        $resolutionImg.textContent = await cb(formData.get('image'))
     }
 }
 
@@ -154,4 +183,53 @@ function viewErrors(errors, clientOrServer) {
 }
 
 
+function sizeImg(size) {
+    let sizeImg = size
 
+    for (let i = 0; i < 2; i++) {
+        sizeImg /= 1024
+    }
+
+    return sizeImg.toFixed(2) + 'MB'
+}
+
+
+// Función para procesar la imagen y obtener su resolución
+const getResolutionFromImage = (file) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const image = new Image();
+            image.src = e.target.result;
+
+            image.onload = () => {
+                const width = image.width;
+                const height = image.height;
+
+                resolve({ width, height });
+            };
+
+            image.onerror = () => {
+                reject(new Error('No se pudo cargar la imagen'));
+            };
+        }
+
+        reader.readAsDataURL(file);
+    })
+}
+
+// Función para manejar el evento de cambio del input file
+const handleImageUpload = async (file) => {
+    if (!file) {
+        return 'No se seleccionó una imagen';
+    }
+
+    try {
+        const { width, height } = await getResolutionFromImage(file);
+        return `${width} x ${height}`;
+    } catch (error) {
+        console.error(error.message);
+        return 'No se pudo cargar la resolución de la imagen';
+    }
+};
